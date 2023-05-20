67e36bc9338035c62def2d8765b099be00e4659d
WillSlye <40750143+WillSlye@users.noreply.github.com>
2023-05-20 12:50:35 -0400

    Update api.py

--- api.py
+++ api.py
@@ -1,10 +1,26 @@
 from flask import Flask
+import sqlite3
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/api')
 def api():
+    # Log the API call into the database
+    log_to_database('/api')
+
     return 'API endpoint'
+
+def log_to_database(endpoint):
+    # Conect to the database
+    conn = sqlite3.connect('logs.db')
+    c = conn.cursor()
+
+    # Insert the endpoint into the logs table
+    c.execute("INSERT INTO logs (endpoint) VALUES (?)", (endpoint,))
+    conn.commit()
+
+    # Close the database connection
+    conn.close()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
