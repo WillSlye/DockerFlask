4cca2070ca7172e39d2af100a625e28ef7aea503
WillSlye <40750143+WillSlye@users.noreply.github.com>
2023-05-19 21:51:22 -0400

    Update api.py

--- api.py
+++ api.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def api():
+    return 'API endpoint'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
