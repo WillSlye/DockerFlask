4912e7ed71d564e4818a374f7108c2da259a84c2
WillSlye <40750143+WillSlye@users.noreply.github.com>
2023-05-20 12:52:00 -0400

    Update microservice.py

--- microservice.py
+++ microservice.py
@@ -1,10 +1,26 @@
 from flask import Flask
+import sqlite3
 
 app = Flask(__name__)
 
 @app.route('/microservice')
 def microservice():
+    # Log the microservice call to the database
+    log_to_database('/microservice')
+
     return 'Microservice endpoint'
+
+def log_to_database(endpoint):
+    # Connect to the database
+    conn = sqlite3.connect('logs.db')
+    c = conn.cursor()
+
+    # Insert the endpoint into the logs table
+    c.execute("INSERT INTO logs (endpoint) VALUES (?)", (endpoint,))
+    conn.commit()
+
+    # Close the database connection
+    conn.close()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
