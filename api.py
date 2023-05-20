d98b818a5afe563494d6203a13d0710a2a198715
WillSlye <40750143+WillSlye@users.noreply.github.com>
2023-05-20 14:47:41 -0400

    Updated api.py

--- api.py
+++ api.py
@@ -3,9 +3,17 @@ import sqlite3
 
 app = Flask(__name__)
 
+def create_table():
+    conn = sqlite3.connect('logs.db')
+    c = conn.cursor()
+    c.execute("CREATE TABLE IF NOT EXISTS logs (endpoint TEXT)")
+    conn.commit()
+    conn.close()
+
 @app.route('/api')
 def api():
     # Log the API call into the database
+    create_table()
     log_to_database('/api')
 
     return 'API endpoint'
@@ -24,3 +32,4 @@ def log_to_database(endpoint):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
